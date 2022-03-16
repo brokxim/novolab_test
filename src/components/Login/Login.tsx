5753c5d390a39e26fb08799f59e557eb9b5ecfc6
ibrohimsoft <ahatkulov98@gmail.com>
2022-03-16 21:39:54 +0500

    fix bug

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,18 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useRef} from 'react';
 import { useNavigate} from 'react-router-dom';
 import {useDispatch,useSelector} from 'react-redux';
 import {updateCount} from '../../redux/post';
 import News from '../News/News';
+import {AiOutlineEyeInvisible} from 'react-icons/ai'
+import {MdOutlineVisibility} from 'react-icons/md'
+
 import './Login.scss';
 function Login() {
      const navigate= useNavigate();
   const[login,setLogin]=useState('');
   const[password,setPassword]=useState('');
   const[flag,setFlag]=useState(false);
+  const ref=useRef<HTMLInputElement>(null);
 
  const dispatch = useDispatch();
 
@@ -32,7 +36,9 @@ function Login() {
       navigate('/news')
      }
     }
-    
+    const [show,setShow]=useState(false);
+    function toggleVisible(){
+      setShow(p=>!p) }
   return <>
   {!flag ?
    <div className="wrapper">
@@ -43,8 +49,14 @@ function Login() {
              <div className="wrapper_item_registration_form">
                <h4 className="enter">Log in</h4>
                <form onSubmit={submitUser} >
-                 <input type="text" placeholder="Login" onChange={(e:any)=>setLogin(e.target.value)} required/>
-                 <input type="password" placeholder="Parol" onChange={(e:any)=>setPassword(e.target.value)} required/>
+                 <input type="text" placeholder="Login" onChange={(e:any)=>setLogin(e.target.value)}  required/>
+                 <div className='d-flex'>
+                    <input ref={ref} className='position-relative' type={show ?'text':'password'} placeholder="Parol" onChange={(e:any)=>setPassword(e.target.value)} autoComplete='off' required/>                    
+                    <span className='onVisible' onClick={toggleVisible}>
+                       {show ? <MdOutlineVisibility/>:<AiOutlineEyeInvisible/>}
+                    </span>  
+                    
+                 </div>
                  <button type='submit' className="kirish"><span>Enter</span></button>
                  <button type='reset' className="reset">reset</button>
                </form>
